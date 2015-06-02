cc7522d999f2457c4d7fbe6e66fe170ef7f3f351
unknown <eduardo.ponte@IDWS101255.DOMINIO.LOCAL>
2015-06-02 13:08:51 +0200

    Step 3: Implement the replicator so that it correctly mediates between replication requests, snapshots and acknowledgements.

--- src/main/scala/kvstore/Replicator.scala
+++ src/main/scala/kvstore/Replicator.scala
@@ -1,8 +1,6 @@
 package kvstore
 
-import akka.actor.Props
-import akka.actor.Actor
-import akka.actor.ActorRef
+import akka.actor.{Cancellable, Props, Actor, ActorRef}
 import scala.concurrent.duration._
 
 object Replicator {
@@ -24,8 +22,9 @@ class Replicator(val replica: ActorRef) extends Actor {
    * The contents of this actor is just a suggestion, you can implement it in any way you like.
    */
 
+  var seqs = Map.empty[Long, Long]
   // map from sequence number to pair of sender and request
-  var acks = Map.empty[Long, (ActorRef, Replicate)]
+  var acks = Map.empty[Long, (ActorRef, Replicate, Cancellable)]
   // a sequence of not-yet-sent snapshots (you can disregard this if not implementing batching)
   var pending = Vector.empty[Snapshot]
   
@@ -36,10 +35,43 @@ class Replicator(val replica: ActorRef) extends Actor {
     ret
   }
 
-  
+
+  def replicate(k: String, v: Option[String], id: Long, msg: Replicate): Unit = {
+    val cancellableRetry = context.system.scheduler.scheduleOnce(200 milliseconds) {
+      self ! Replicate(k, v, id)
+    }
+
+    seqs.get(id) match {
+      case Some(seq) => {
+        val ack = acks(seq)
+        acks.updated(seq,(ack._1, ack._2, cancellableRetry)) // replace with new cancellable
+        replica ! Snapshot(k, v, seq)
+      }
+      case None => {
+        acks += _seqCounter -> ((sender, msg, cancellableRetry))
+        seqs += ((id, _seqCounter))
+        replica ! Snapshot(k, v, _seqCounter)
+        nextSeq
+      }
+    }
+  }
+
+  def confirmReplication(seq: Long): Unit = {
+    acks.get(seq) match {
+      case Some(ack) => {
+        acks -= seq
+        ack._3.cancel
+        ack._1 ! Replicated(ack._2.key, ack._2.id)
+      }
+      case None =>
+    }
+  }
+
   /* TODO Behavior for the Replicator. */
   def receive: Receive = {
-    case _ =>
+    case replicateMsg @ Replicate(k, v, id) => replicate(k, v, id, replicateMsg)
+    case SnapshotAck(key, seq) => confirmReplication(seq)
   }
+
 
 }
